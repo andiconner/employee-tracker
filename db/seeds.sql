ebc4190f9879583ed8d244b1057f6d034a6c5125
Andi Conner <andiconner@icloud.com>
2022-01-02 14:52:43 -0600

    seeds department

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (name)
+VALUES
+  ('Sales'),
+  ('Engineering'),
+  ('Finance'),
+  ('Legal'),
+  ('Marketing'),
+  ('Service');
+
+INSERT INTO role (title, salary,department_id)
+VALUES
+('Sales Rep', 25000, 1);
